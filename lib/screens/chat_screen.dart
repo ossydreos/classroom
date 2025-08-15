f0ad31385e61a6a75c3a61e31085045b9191d176
Youri <osdreos@gmail.com>
2025-08-15 09:24:14 +0200

    ajout fonction log out sur chat_screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -7,21 +8,42 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Chat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(Icons.more_vert),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if(itemIdentifier == 'logout'){
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          ),
+        ],
+      ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Messages(),
-            ),
-          ],
-        ),
+        child: Column(children: <Widget>[Expanded(child: Messages())]),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance.collection('chats/klSHI7dxjkq4Lakm5Rk8/messages').add({
-            'text' : 'Ajouté avec le bouton'
-          });
+          FirebaseFirestore.instance
+              .collection('chats/klSHI7dxjkq4Lakm5Rk8/messages')
+              .add({'text': 'Ajouté avec le bouton'});
         },
       ),
     );
