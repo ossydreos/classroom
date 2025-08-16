95260785a5767e5f8041a7434c44b3337c985434
Youri <osdreos@gmail.com>
2025-08-16 15:36:14 +0200

    ajout fonctions pour notifications

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,48 +1,67 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/messages.dart';
-
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print(message.data);
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print(message.data);
+    });
+    fbm.subscribeToTopic('chat');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Chat'),
+        title: const Text('FlutterChat'),
         actions: [
           DropdownButton(
-            icon: Icon(Icons.more_vert, color: Colors.white,),
+            underline: Container(),
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
             items: [
               DropdownMenuItem(
                 value: 'logout',
-                child: Container(
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.exit_to_app, color: Colors.black,),
-                      SizedBox(width: 8),
-                      Text('Logout'),
-                    ],
-                  ),
+                child: Row(
+                  children: const [
+                    Icon(Icons.exit_to_app),
+                    SizedBox(width: 8),
+                    Text('Logout'),
+                  ],
                 ),
               ),
             ],
             onChanged: (itemIdentifier) {
-              if(itemIdentifier == 'logout'){
+              if (itemIdentifier == 'logout') {
                 FirebaseAuth.instance.signOut();
               }
             },
           ),
         ],
       ),
-      body: Container(
-        child: Column(children: <Widget>[
-            Expanded(
-              child: Messages(),
-            ),
-            NewMessage(),
-          ]),
+      body: Column(
+        children: <Widget> [
+          Expanded(child: Messages()),
+          NewMessage(),
+        ],
       ),
     );
   }
