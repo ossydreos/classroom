6a747651134f2fe62f73f140070ce8eb301d148c
Youri <osdreos@gmail.com>
2025-08-17 10:55:50 +0200

    ajout image et changement nom app + fix divers

--- functions/index.js
+++ functions/index.js
@@ -7,22 +7,26 @@ setGlobalOptions({region: "europe-west3"});
 
 initializeApp();
 
-exports.myFunction = onDocumentCreated("chat/{message}", async (event) => {
-  const snap = event.data;
-  if (!snap) return null;
+exports.myFunction = onDocumentCreated(
+    "rooms/{roomId}/chat/{message}",
+    async (event) => {
+      const snap = event.data;
+      if (!snap) return null;
 
-  const data = snap.data() || {};
-  const title = data.username || "Nouveau message";
-  const body = data.text || "";
+      const data = snap.data() || {};
+      const roomId = event.params.roomId;
+      const title = `${data.username || "Nouveau message"} (${roomId})`;
+      const body = data.text || "";
 
-  console.log("Trigger OK:", {title, body});
+      console.log("Trigger OK:", {title, body});
 
-  const messageId = await getMessaging().send({
-    topic: "chat",
-    notification: {title, body},
-    android: {notification: {clickAction: "FLUTTER_NOTIFICATION_CLICK"}},
-  });
+      const messageId = await getMessaging().send({
+        topic: "chat",
+        notification: {title, body},
+        android: {notification: {clickAction: "FLUTTER_NOTIFICATION_CLICK"}},
+      });
 
-  console.log("FCM sent:", messageId);
-  return null;
-});
+      console.log("FCM sent:", messageId);
+      return null;
+    },
+);
